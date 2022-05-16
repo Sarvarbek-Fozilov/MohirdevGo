740e5bc299b497f9580854e846bd92763e50a4f9
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-16 20:24:07 +0500

    Bu darsda if else ni ko'rib chiqdim va dars davomida bir qancha examplar orqalik if else darsni tushundim

---
+++ 2 If else/dars1/ifelse.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+func main(){
+	/*
+	a:=10
+	b:=
+	if a>b {
+		fmt.Println("A katta!!")
+	} else {
+		fmt.Println("B katta !!")
+	}
+	*/
+	/*
+	var a int
+	var b int
+	fmt.Println("A sonini kiriting: ")
+	fmt.Scanln(&a)
+
+	fmt.Println("b sonini kiriting: ")
+	fmt.Scanln(&b)
+
+	if a>b {
+		fmt.Println("A katta")
+	} else if a == b {
+		fmt.Println("A B teng")
+	} else {
+		fmt.println("B katta")
+	}
+	*/
+	if son:=23; son>0{
+		fmt.Println("Son pozititv")
+	} else {
+		fmt.Println("Son negativ")
+	}
+	
+}
