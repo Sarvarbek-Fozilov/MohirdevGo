ff74a1d752b39057d5d3632d3e1cbd37ed227164
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-17 22:50:47 +0500

    For loopida tsikl ni tushundim

---
+++ Forloop/forloop/dars1.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// For loop
+
+	for sanoq :=0; sanoq<=5; sanoq ++ {
+		println("Sanoq: ",sanoq)
+	}
+
+	for son:=10;son>=0;son-=1{
+		println("Son: ",son)
+	}
+
+	for number:=0; number <10; number++{
+		println("Number: ", number)
+	}
+}
