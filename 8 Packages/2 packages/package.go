8798b3f460bbf1c58c1739f984cac8915a76b7ed
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-23 20:07:45 +0500

    git hubdan packet import qilish uni qayta nomlash

---
+++ 8 Packages/2 packages/package.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"/*agar package larni nomi bir xil bolib qolsa  
+	uni ozimiz qoygan nom orqalik ham chaqrishmiz mumkin.buninng uchun 
+	 package oldidan nom qoyamiz va shu nom ilan chaqiramiz
+	 misol uchun: ||||  Rang "github.com/fatih/color"*/
+
+)
+func main(){
+	color.Red("bu qizil yozuvda chiqadi") // Rang.Red("Bu qizil yozuvda chiqadi")
+	color.Cyan("Cyanda chiqadi")       //Rang.Cyan("Cyanda chiqadi")
+	fmt.Print()
+
+}
