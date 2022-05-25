e3ec7697904f16b0e4b0ea31c442c01130ebc736
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 22:32:49 +0500

    pointerlarni,ozgaruvchilar qiymatni pointer orqalik ozlashtrish,&,* ni farqi va ishlatish joyini, main funksyasida boshqa  ozimiz yaratgan funksyasini foydalanish

--- 9 Function/2 pointers/pointers.go
+++ 9 Function/2 pointers/pointers.go
@@ -1,11 +1,26 @@
 package main
 
 import "fmt"
-func main(){
-	son1:=12
-	son2:=10
-	var p=&son1
-	fmt.Println(son1==son2)
-	fmt.Println(son2==*p)
 
+// * Pointers
+
+func main() {
+ number := 1
+ example(&number)                //  number bu shunchaki o'zgaruvchi uni pastdagi example funksyasida ishlata olmaymiz ,ishlatish uchun esa uning adresini  examplega jonatish orqalik uning qiymatidan foydalanishmiz mumkn
+ fmt.Println("Number: ", number) // pastdagi example orqali o'zgartrgan qiymatimizni chiqaradi
+
+ /*
+  var p *int
+  i := 42
+  p = &i
+  fmt.Println(p)  // pinterning ramdagi manzilini ekranga chiqaradi
+  fmt.Println(*p) // pointerning o'zlashtrgan qiymatini chiqaradi
+ */
+
+}
+
+func example(son *int) {
+ fmt.Println("Adresi:", son)
+ fmt.Println("Son:", *son)
+ *son = 23 // Bunda numberni ichga kirib qiymatni o'zgartradi
 }
